bbe31bb4bef1f0972e5e21dfbc72d14f1d4231f2
Kevin Liang <kevin.liang@duke.edu>
2017-01-20 14:58:36 -0500

    Added a simple convnet cnn. Started test suite.

--- Development/tests.py
+++ Development/tests.py
@@ -14,8 +14,28 @@ sys.path.append('../')
 import tensorflow as tf
 import numpy as np
 
+from Networks.convnet import convnet
 from Networks.resnet import resnet
-#from Networks.faster_rcnn_networks import rpn, roi_proposal, fast_rcnn
+from Networks.faster_rcnn_networks import rpn, roi_proposal, fast_rcnn
+
+# Global Dictionary of Flags
+flags = {
+    'data_directory': '../Data/MNIST/',
+    'save_directory': '../Logs/summaries/',
+    'model_directory': 'resnet101/',
+    'restore': False,
+    'restore_file': 'start.ckpt',
+    'datasets': 'MNIST',
+    'image_dim': 28,
+    'hidden_size': 10,
+    'num_classes': 10,
+    'batch_size': 100,
+    'display_step': 200,
+    'weight_decay': 1e-7,
+    'lr_decay': 0.999,
+    'lr_iters': [(5e-3, 5000), (5e-3, 7500), (5e-4, 10000), (5e-5, 10000)],
+    'anchor_scales': [8,16,32]
+}
 
 class faster_rcnn_tests():
     def __init__(self):
@@ -23,21 +43,42 @@ class faster_rcnn_tests():
         self.sess = tf.InteractiveSession()
         
     def test_all(self):
+        self.test_convnet_dims()
         self.test_resnet_dims()
+        
+    def test_convnet_dims(self):
+        filter_sizes = (3,3,3,3)
+        output_channels = (32,64,64,128)
+        strides = (1,2,1,2)
+        cnn = convnet(self.x, filter_sizes, output_channels, strides)
+        featureMaps = cnn.get_output()
+        
+        init = tf.global_variables_initializer()
+        self.sess.run(init)
+                
+        test_image = np.random.randint(0,256,[1,128,128,3])
+        feat_val = self.sess.run(featureMaps,feed_dict={self.x:test_image})
+        feat_val = np.array(feat_val)
+        
+        print(feat_val.shape)
+        assert np.all(feat_val.shape == np.array([1,32,32,128]))
     
     def test_resnet_dims(self):
         cnn = resnet(50, self.x)
         featureMaps = cnn.get_output()
+        
         init = tf.global_variables_initializer()
         self.sess.run(init)
         
         test_image = np.random.randint(0,256,[1,128,128,3])
-        feat_val = self.sess.run([featureMaps],feed_dict={self.x:test_image})
+        feat_val = self.sess.run(featureMaps,feed_dict={self.x:test_image})
         feat_val = np.array(feat_val)
         
         print(feat_val.shape)
-        assert np.all(feat_val.shape == [1,4,4,2048])
+        assert np.all(feat_val.shape == np.array([1,4,4,2048]))
     
+
+        
         
 def main():
     print("Initiating Tests")
---
+++ Networks/convnet.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 20 12:19:17 2017
+
+@author: Kevin Liang
+
+Basic ConvNet Architecture: Convolutional feature extractor
+
+Support for arbitrary number of layers
+
+Built on TensorBase: https://github.com/kevinjliang/TensorBase
+"""
+import sys
+sys.path.append('../')
+
+from Lib.TensorBase.tensorbase.base import Layers
+
+import numpy as np
+import tensorflow as tf
+
+
+class convnet:    
+    def __init__(self, x, filter_sizes, output_channels, strides=None):
+        self.filter_sizes = filter_sizes
+        self.depth = len(self.filter_sizes)
+        self.output_channels = output_channels
+        if strides is not None:
+            self.strides = strides
+        else:
+            self.strides = np.ones([1,self.depth])
+            
+        self.network = self._network(x)
+        
+    def _network(self, x):
+        conv_layers = Layers(x)
+            
+        # Convolutional layers
+        scope = 'convnet' + str(self.depth)
+        with tf.variable_scope(scope):
+            for l in range(self.depth):
+                conv_layers.conv2d(filter_size=self.filter_sizes[l], 
+                                   output_channels=self.output_channels[l], 
+                                   stride=self.strides[l],
+                                   padding='SAME',
+                                   b_value=None)
+        
+        return conv_layers
+        
+    def get_output(self):
+        return self.network.get_output()
+        
+    def get_feat_stride(self):
+        return np.prod(self.strides)
+        
