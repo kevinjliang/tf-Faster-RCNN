ce6d4187bc7d21b84e77c28dafe159126a6a496f
Kevin Liang <kevin.liang@duke.edu>
2017-01-23 10:00:24 -0500

    Python module fixing. Added __init__ file to Lib/

--- Lib/nms_wrapper.py
+++ Lib/nms_wrapper.py
@@ -13,9 +13,9 @@ Created on Fri Jan 20 15:35:33 2017
 # Written by Ross Girshick
 # --------------------------------------------------------
 
-from fast_rcnn_config import cfg
-from nms.gpu_nms import gpu_nms
-from nms.cpu_nms import cpu_nms
+from .fast_rcnn_config import cfg
+from .nms.gpu_nms import gpu_nms
+from .nms.cpu_nms import cpu_nms
 
 def nms(dets, thresh, force_cpu=False):
     """Dispatch to either CPU or GPU NMS implementations."""
