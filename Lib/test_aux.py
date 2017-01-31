d57724dfd10049200e452271be4b93b1840a7982
Kevin Liang <kevin.liang@duke.edu>
2017-01-31 13:22:01 -0500

    Working out File IO for test

--- Lib/test_aux.py
+++ Lib/test_aux.py
@@ -118,8 +118,73 @@ def apply_nms(all_boxes, thresh):
                 continue
             nms_boxes[cls_ind][im_ind] = dets[keep, :].copy()
     return nms_boxes    
-    
-    
 
     
+import matplotlib.pyplot as plt
+import cv2
+from Lib.fast_rcnn_config import cfg, get_output_dir
+import Pickle
+import os
     
+def test_net(sess, net, imdb, weights_filename , max_per_image=300, thresh=0.05, vis=False):
+    """Test a Fast R-CNN network on an image database."""
+    num_images = len(imdb.image_index)
+    # all detections are collected into:
+    #    all_boxes[cls][image] = N x 5 array of detections in
+    #    (x1, y1, x2, y2, score)
+    all_boxes = [[[] for _ in range(num_images)]
+                 for _ in range(imdb.num_classes)]
+
+    output_dir = get_output_dir(imdb, weights_filename)
+    # timers
+#    _t = {'im_detect' : Timer(), 'misc' : Timer()}
+
+    for i in range(num_images):
+        box_proposals = None
+
+        im = cv2.imread(imdb.image_path_at(i))
+#        _t['im_detect'].tic()
+        scores, boxes = im_detect(sess, net, im, box_proposals)
+#        _t['im_detect'].toc()
+
+#        _t['misc'].tic()
+        if vis:
+            image = im[:, :, (2, 1, 0)]
+            plt.cla()
+            plt.imshow(image)
+
+        # skip j = 0, because it's the background class
+        for j in range(1, imdb.num_classes):
+            inds = np.where(scores[:, j] > thresh)[0]
+            cls_scores = scores[inds, j]
+            cls_boxes = boxes[inds, j*4:(j+1)*4]
+            cls_dets = np.hstack((cls_boxes, cls_scores[:, np.newaxis])) \
+                .astype(np.float32, copy=False)
+            keep = nms(cls_dets, cfg.TEST.NMS)
+            cls_dets = cls_dets[keep, :]
+            if vis:
+                vis_detections(image, imdb.classes[j], cls_dets)
+            all_boxes[j][i] = cls_dets
+        if vis:
+           plt.show()
+        # Limit to max_per_image detections *over all classes*
+        if max_per_image > 0:
+            image_scores = np.hstack([all_boxes[j][i][:, -1]
+                                      for j in range(1, imdb.num_classes)])
+            if len(image_scores) > max_per_image:
+                image_thresh = np.sort(image_scores)[-max_per_image]
+                for j in range(1, imdb.num_classes):
+                    keep = np.where(all_boxes[j][i][:, -1] >= image_thresh)[0]
+                    all_boxes[j][i] = all_boxes[j][i][keep, :]
+#        _t['misc'].toc()
+#
+#        print('im_detect: {:d}/{:d} {:.3f}s {:.3f}s' \
+#              .format(i + 1, num_images, _t['im_detect'].average_time,
+#                      _t['misc'].average_time))
+
+    det_file = os.path.join(output_dir, 'detections.pkl')
+    with open(det_file, 'wb') as f:
+        Pickle.dump(all_boxes, f)
+
+    print('Evaluating detections')
+    imdb.evaluate_detections(all_boxes, output_dir)
