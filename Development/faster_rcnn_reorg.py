tuned hyperparameters and fixing mAP calculation

Development/faster_rcnn_reorg.py
@@ -13,7 +13,7 @@ import sys
 
 sys.path.append('../')
 
-from Lib.TensorBase.tensorbase.base import Model, Data
+from Lib.TensorBase.tensorbase.base import Model, Data, Layers
 from Lib.test_aux import test_net, vis_detections
 
 from Networks.convnet import convnet
@@ -125,11 +125,21 @@ class FasterRcnnConv5(Model):
         self.fast_rcnn_bbox_loss = self.fast_rcnn_net['TRAIN'].get_fast_rcnn_bbox_loss()
 
         # Total Loss
-        self.cost = tf.reduce_sum(self.rpn_cls_loss + self.rpn_bbox_loss + self.fast_rcnn_cls_loss +
-                                  self.fast_rcnn_bbox_loss)
+        self.cost = tf.reduce_sum(self.rpn_cls_loss + self.rpn_bbox_loss + 100 * self.fast_rcnn_cls_loss)
 
         # Optimization operation
-        self.optimizer = tf.train.AdamOptimizer().minimize(self.cost)
+        self.optimizer = tf.train.AdamOptimizer(learning_rate=0.0001).minimize(self.cost)
+
+        # Classifcation Objective
+        if self.flags['restore'] is False:
+            class_model = Layers(self.cnn['TRAIN'].get_output())
+            class_model.flatten()
+            class_model.fc(1024)
+            class_model.fc(11, activation_fn=None)
+            self.class_cost = tf.nn.sparse_softmax_cross_entropy_with_logits(class_model.get_output(),
+                                                                            self.gt_boxes['TRAIN'][:, 4])
+
+            self.optimizer_class = tf.train.AdamOptimizer(learning_rate=0.0001).minimize(self.class_cost)
 
     def test_print_image(self):
         """ Read data through self.sess and plot out """
@@ -143,6 +153,11 @@ class FasterRcnnConv5(Model):
             vis_detections(np.squeeze(image[0]), str(gt_boxes[0][4]), gt_boxes, bboxes)
             print('Image Num: %d' % i)
 
+    def _run_train_class_iter(self):
+        """ Run training iteration"""
+        loss, _ = self.sess.run([self.class_cost, self.optimizer_class])
+        return loss
+
     def _run_train_iter(self):
         """ Run training iteration"""
         summary, _ = self.sess.run([self.merged, self.optimizer])
@@ -150,8 +165,21 @@ class FasterRcnnConv5(Model):
 
     def _record_train_metrics(self):
         """ Record training metrics """
-        loss, rois = self.sess.run([self.cost, self.roi_proposal_net['TRAIN'].get_rois()])
+        loss, cls_score, lab, loss_clss = self.sess.run([self.cost, self.fast_rcnn_net['TRAIN'].get_cls_score(),
+                                                         self.roi_proposal_net['TRAIN'].get_labels(),
+                                                         self.fast_rcnn_cls_loss])
+        np.set_printoptions(precision=2)
+        print(cls_score)
+        print(lab)
+        print('Class Loss: %f' % loss_clss)
         self.print_log('Step %d: loss = %.6f' % (self.step, loss))
+
+    def train_class(self,):
+        iterations = int(np.ceil(self.num_images['TRAIN'] / self.flags['batch_size']) * 5)
+        for i in tqdm(range(iterations)):
+            loss = self._run_train_class_iter()
+            if i % 550 == 0:
+                self.print_log('Step %d: loss = %.6f' % (i, loss))
 
     def train(self):
         """ Run training function. Save model upon completion """
@@ -230,6 +258,7 @@ def main():
         flags['restore_file'] = 'part_' + str(args['file_epoch']) + '.ckpt.meta'
     model = FasterRcnnConv5(flags)
     if int(args['train']) == 1:
+        model.train_class()
         model.train()
     if int(args['eval']) == 1:
         model.test()

Lib/Datasets/eval_clutteredMNIST.py
@@ -41,7 +41,7 @@ def voc_ap(rec, prec):
     return ap
 
 
-def cluttered_mnist_eval(test_image_object, test_directory, ovthresh=0.5):
+def cluttered_mnist_eval(test_image_object, test_directory, num_images, ovthresh=0.5):
     """
     Evalulates predicted detections on cluttered MNIST dataset
     :param test_image_object: array, obj[cls][image] = N x 5 [x1, y1, x2, y2, cls_score]
@@ -50,20 +50,20 @@ def cluttered_mnist_eval(test_image_object, test_directory, ovthresh=0.5):
     :return: class_metrics: list, each index is a digit class which holds a tuple of rec, prec, and ap
     """
     # Get Ground Truth numbers for classes
-    total_num = np.zeros([11])
+    total_num = np.zeros([1])
     print('Loading Grouth Truth Data to count number of grouth truth per class')
-    for x in tqdm(range(len(test_image_object[0]))):
+    for x in tqdm(range(num_images)):  # number of test data
         key = 'img' + str(x)
         gt_boxes = np.loadtxt(test_directory + 'Annotations/' + key + '.txt', ndmin=2)
         for g in range(gt_boxes.shape[0]):
             label = int(gt_boxes[g, 4])
-            total_num[label+1] += 1
+            total_num[label] += 1
 
     # Designate arrays to hold ap for each class
     class_metrics = list()
 
     # Calculate IoU for all classes and all images
-    for c in range(len(test_image_object)):  # loop through all classes (skip background class)
+    for c in range(1, len(test_image_object)):  # loop through all classes (skip background class)
         print('Now Calculating average precision for class: %d' % c)
         class_tp = list()
         class_fp = list()
@@ -120,19 +120,19 @@ def cluttered_mnist_eval(test_image_object, test_directory, ovthresh=0.5):
                     fp[d] = 1.
 
             # Add scores from all dets in one image to the class true positives and false positives
-            class_tp.append(tp)
-            class_fp.append(fp)
+            class_tp.extend(tp)
+            class_fp.extend(fp)
 
         # compute precision recall
         fp = np.cumsum(class_fp)
         tp = np.cumsum(class_tp)
         rec = tp / float(total_num[c])
+        print(rec)
 
         # avoid divide by zero in case the first detection matches a difficult
         # ground truth
         prec = tp / np.maximum(tp + fp, np.finfo(np.float64).eps)
         ap = voc_ap(rec, prec)
-        class_metrics.append((rec, prec, ap))
-
-    print('Finished Calculating average precisions.')
+        class_metrics.append(ap)
+    print('Mean Average Precision: %f' % np.mean(class_metrics))
     return class_metrics