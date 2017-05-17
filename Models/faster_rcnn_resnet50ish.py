Batch normalization is_training scope fixes

Models/faster_rcnn_resnet50ish.py
@@ -4,7 +4,7 @@ Created on Sat Dec 31 13:22:36 2016
 
 @author: Kevin Liang
 
-Faster R-CNN model using ResNet50 as the convolutional feature extractor. 
+Faster R-CNN model using ResNet50 V2 as the convolutional feature extractor. 
 Option to use ImageNet pre-trained weights
 
 Note: We take the feature maps before the last ResNet block
@@ -18,7 +18,7 @@ from Lib.faster_rcnn_config import cfg, cfg_from_file
 from Lib.test_aux import test_net
 from Lib.train_aux import randomize_training_order, create_feed_dict
 
-from Networks.resnet50V2_reduced import resnet50V2_reduced
+from Networks.resnet50V2_reduced import resnet50V2_reduced, resnet_arg_scope
 from Networks.faster_rcnn_networks import rpn, roi_proposal, fast_rcnn
 
 from tensorflow.contrib.slim.python.slim.nets import resnet_utils
@@ -30,7 +30,7 @@ import argparse
 import os
 
 slim = tf.contrib.slim
-resnet_arg_scope = resnet_utils.resnet_arg_scope
+# resnet_arg_scope = resnet_utils.resnet_arg_scope
 
 # Global Dictionary of Flags: Populated in main() with cfg defaults
 flags = {}
@@ -45,7 +45,6 @@ class FasterRcnnRes50(Model):
    
         self.print_log(dictionary)
         self.print_log(flags_input)
-        self.threads, self.coord = Data.init_threads(self.sess)
 
     def _data(self):
         # Data list for each split
@@ -78,21 +77,21 @@ class FasterRcnnRes50(Model):
 
         # Train network
         with tf.variable_scope("model"):
-            with slim.arg_scope(resnet_arg_scope()):
-                self._faster_rcnn(self.x['TRAIN'], self.gt_boxes['TRAIN'], self.im_dims['TRAIN'], 'TRAIN')
+            self._faster_rcnn(self.x['TRAIN'], self.gt_boxes['TRAIN'], self.im_dims['TRAIN'], 'TRAIN')
 
         with tf.variable_scope("model", reuse=True):
-            with slim.arg_scope(resnet_arg_scope()):
-                self._faster_rcnn(self.x['EVAL'], None, self.im_dims['EVAL'], 'EVAL')
+            self._faster_rcnn(self.x['EVAL'], None, self.im_dims['EVAL'], 'EVAL')
 
     def _faster_rcnn(self, x, gt_boxes, im_dims, key):
         # VALID and TEST are both evaluation mode
         eval_mode = True if (key == 'EVAL') else False
-          
-        # CNN Feature extractor
-        feature_maps = resnet50V2_reduced(x, is_training=(not eval_mode))
-        # CNN downsampling factor
-        _feat_stride = 16           
+        
+        with slim.arg_scope(resnet_arg_scope(is_training=(not eval_mode))):
+            # CNN Feature extractor
+            feature_maps = resnet50V2_reduced(x, is_training=(not eval_mode))
+
+            # CNN downsampling factor
+            _feat_stride = 16           
         
         # Region Proposal Network (RPN)
         self.rpn_net[key] = rpn(feature_maps, gt_boxes, im_dims, _feat_stride, eval_mode)
@@ -119,8 +118,14 @@ class FasterRcnnRes50(Model):
         learning_rate = tf.train.exponential_decay(learning_rate=self.lr, global_step=self.step, 
                                                    decay_steps=decay_steps, decay_rate=cfg.TRAIN.LEARNING_RATE_DECAY, staircase=True)
                 
+        # Update dependencies for batch normalization
+        update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+        
+        # Select only training BN ops        
+        update_ops = [var for var in update_ops if 'model' in var.name.split('/')]
         # Optimizer: ADAM
-        self.optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate, epsilon=0.1).minimize(self.cost)
+        with tf.control_dependencies(update_ops):
+            self.optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate, epsilon=0.1).minimize(self.cost)
         
         
     def _summaries(self):
@@ -183,7 +188,7 @@ class FasterRcnnRes50(Model):
 
     def evaluate(self, test=True):
         """ Evaluate network on the validation set. """
-        key = 'TEST' if test is True else 'VALID'
+        key = 'TEST' if test else 'VALID'
 
         print('Detecting images in %s set' % key)
 

Networks/faster_rcnn_networks.py
@@ -29,9 +29,9 @@ import tensorflow as tf
 
 class rpn:
     '''
-    Region Proposal Network (RPN): From the convolutional feature maps
-    (TensorBase Layers object) of the last layer, generate bounding boxes
-    relative to anchor boxes and give an "objectness" score to each
+    Region Proposal Network (RPN): From the convolutional feature maps of the 
+    last layer, generate bounding boxes relative to anchor boxes and give an 
+    "objectness" score to each
 
     In evaluation mode (eval_mode==True), gt_boxes should be None.
     '''
@@ -58,25 +58,36 @@ class rpn:
         with tf.variable_scope('rpn'):
             # Spatial windowing
             for i in range(len(cfg.RPN_OUTPUT_CHANNELS)):
-                features = tcl.conv2d(inputs=features, num_outputs=cfg.RPN_OUTPUT_CHANNELS[i],
-                                      kernel_size=cfg.RPN_FILTER_SIZES[i])
-
+                # 2D Conv -> Batch Normalization -> ReLU
+                features = tcl.conv2d(inputs=features, 
+                                      num_outputs=cfg.RPN_OUTPUT_CHANNELS[i],
+                                      kernel_size=cfg.RPN_FILTER_SIZES[i], 
+                                      activation_fn=tf.nn.relu,
+                                      normalizer_fn=tcl.batch_norm, 
+                                      normalizer_params={'is_training':not self.eval_mode})
+                
+            # Box-classification layer (objectness)
             with tf.variable_scope('cls'):
-                # Box-classification layer (objectness)
-                self.rpn_cls_score = tcl.conv2d(inputs=features, num_outputs=_num_anchors*2, kernel_size=1,
+                # Only 2D Conv
+                self.rpn_cls_score = tcl.conv2d(inputs=features, 
+                                                num_outputs=_num_anchors*2, 
+                                                kernel_size=1,
                                                 activation_fn=None)
 
+            # Anchor Target Layer (anchors and deltas)
             with tf.variable_scope('target'):
                 # Only calculate targets in train mode. No ground truth boxes in evaluation mode
                 if self.eval_mode is False:
-                    # Anchor Target Layer (anchors and deltas)
                     self.rpn_labels, self.rpn_bbox_targets, self.rpn_bbox_inside_weights, self.rpn_bbox_outside_weights = \
                         anchor_target_layer(rpn_cls_score=self.rpn_cls_score, gt_boxes=self.gt_boxes, im_dims=self.im_dims,
                                             _feat_stride=self._feat_stride, anchor_scales=self.anchor_scales)
 
+            # Bounding-Box regression layer (bounding box predictions)
             with tf.variable_scope('bbox'):
-                # Bounding-Box regression layer (bounding box predictions)
-                self.rpn_bbox_pred = tcl.conv2d(inputs=features, num_outputs=_num_anchors*4, kernel_size=1,
+                # Only 2D Conv
+                self.rpn_bbox_pred = tcl.conv2d(inputs=features, 
+                                                num_outputs=_num_anchors*4, 
+                                                kernel_size=1,
                                                 activation_fn=None)
 
     # Get functions
@@ -204,15 +215,25 @@ class fast_rcnn:
             with tf.variable_scope('fc'):
                 features = tcl.flatten(pooled_features)
                 for i in range(len(cfg.FRCNN_FC_HIDDEN)):
-                    features = tcl.fully_connected(inputs=features, num_outputs=cfg.FRCNN_FC_HIDDEN[i])
+                    # FC -> Batch Normalization -> ReLU [Commented out:-> Dropout]
+                    features = tcl.fully_connected(inputs=features, 
+                                                   num_outputs=cfg.FRCNN_FC_HIDDEN[i],
+                                                   activation_fn=tf.nn.relu,
+                                                   normalizer_fn=tcl.batch_norm, 
+                                                   normalizer_params={'is_training':not self.eval_mode})
+#                    features = tcl.dropout(inputs=features,
+#                                           keep_prob=cfg.FRCNN_DROPOUT_KEEP_RATE,
+#                                           is_training=not self.eval_mode)                    
 
             # Classifier score
             with tf.variable_scope('cls'):
-                self.rcnn_cls_score = tcl.fully_connected(inputs=features, num_outputs=self.num_classes,
+                self.rcnn_cls_score = tcl.fully_connected(inputs=features, 
+                                                          num_outputs=self.num_classes,
                                                           activation_fn=None)
             # Bounding Box refinement
             with tf.variable_scope('bbox'):
-                self.rcnn_bbox_refine = tcl.fully_connected(inputs=features, num_outputs=self.num_classes*4,
+                self.rcnn_bbox_refine = tcl.fully_connected(inputs=features, 
+                                                            num_outputs=self.num_classes*4,
                                                             activation_fn=None)
 
     # Get functions

Networks/resnet50V2_reduced.py
@@ -18,7 +18,6 @@ import tensorflow as tf
 from tensorflow.contrib.slim.python.slim.nets import resnet_utils
 
 slim = tf.contrib.slim
-resnet_arg_scope = resnet_utils.resnet_arg_scope
 
 @slim.add_arg_scope
 def bottleneck(inputs, depth, depth_bottleneck, stride, rate=1, outputs_collections=None, scope=None):
@@ -64,3 +63,39 @@ def resnet50V2_reduced(inputs, is_training=True, output_stride=None, include_roo
     with slim.arg_scope([slim.batch_norm], is_training=is_training) as scope:
         net = slim.batch_norm(net, activation_fn=tf.nn.relu, scope='postnorm')
     return net
+
+def resnet_arg_scope(weight_decay=0.0001,
+                     batch_norm_decay=0.997,
+                     batch_norm_epsilon=1e-5,
+                     batch_norm_scale=True,
+                     is_training=True):
+  """Defines the default ResNet arg scope.
+  Args:
+    weight_decay: The weight decay to use for regularizing the model.
+    batch_norm_decay: The moving average decay when estimating layer activation
+      statistics in batch normalization.
+    batch_norm_epsilon: Small constant to prevent division by zero when
+      normalizing activations by their variance in batch normalization.
+    batch_norm_scale: If True, uses an explicit `gamma` multiplier to scale the
+      activations in the batch normalization layer.
+  Returns:
+    An `arg_scope` to use for the resnet models.
+  """
+  batch_norm_params = {
+      'decay': batch_norm_decay,
+      'epsilon': batch_norm_epsilon,
+      'scale': batch_norm_scale,
+      'is_training': is_training,
+      'updates_collections': tf.GraphKeys.UPDATE_OPS,
+  }
+
+  with slim.arg_scope(
+      [slim.conv2d],
+      weights_regularizer=slim.l2_regularizer(weight_decay),
+      weights_initializer=slim.variance_scaling_initializer(),
+      activation_fn=tf.nn.relu,
+      normalizer_fn=slim.batch_norm,
+      normalizer_params=batch_norm_params):
+    with slim.arg_scope([slim.batch_norm], **batch_norm_params):
+      with slim.arg_scope([slim.max_pool2d], padding='SAME') as arg_sc:
+        return arg_sc